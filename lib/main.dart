d61a6b34b2c98238e0e471f50d3d2ec27412437a
Asher <asheressani@gmail.com>
2024-12-09 21:50:32 +0500

    Added Gemini and env config!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:communico_frontend/di/service_locator.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 
 import 'presentation/communico.dart';
@@ -7,6 +8,7 @@ import 'presentation/communico.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ServiceLocator.configureServiceLocator();
-  Gemini.init(apiKey: "apiKey");
+  await dotenv.load();
+  Gemini.init(apiKey: dotenv.env['GEMINI_API_KEY'].toString());
   runApp(const Communico());
 }
