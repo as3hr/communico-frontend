a7dbd9e522b78957376c59d4923e9ec71e7f9e48
Asher <asheressani@gmail.com>
2024-12-16 03:44:04 +0500

    Updated workflow:6

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,7 +1,7 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:socket_io_client/socket_io_client.dart';
 
-final baseUrl = dotenv.env['base_url'].toString();
+// final baseUrl = dotenv.env['base_url'].toString();
+const baseUrl = "https://communico.as3hr.dev/api";
 const tokenKey = "API_TOKEN_KEY";
 var socket = io(baseUrl, {
   'transports': ['websocket'],
