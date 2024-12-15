6fcd69a3d6caa5f0e64d6f5c80c7d7bcb82273f8
Asher <asheressani@gmail.com>
2024-12-15 17:15:34 +0500

    Frontend-deployment:latest

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,6 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:socket_io_client/socket_io_client.dart';
 
-const baseUrl = "http://localhost:5000";
+final baseUrl = dotenv.env['base_url'].toString();
 const tokenKey = "API_TOKEN_KEY";
 var socket = io(baseUrl, {
   'transports': ['websocket'],
