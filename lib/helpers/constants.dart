0b2708cc4b79f6932751cef9c4ab928360ab5ec9
Asher <asheressani@gmail.com>
2024-12-16 16:05:27 +0500

    Updated Socket URL!

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -3,7 +3,8 @@ import 'package:socket_io_client/socket_io_client.dart';
 // final baseUrl = dotenv.env['base_url'].toString();
 const baseUrl = "https://communico.as3hr.dev/api";
 const tokenKey = "API_TOKEN_KEY";
-var socket = io(baseUrl, {
+var socket = io("https://communico.as3hr.dev", {
   'transports': ['websocket'],
   'autoConnect': true,
+  'debug': true,
 });
