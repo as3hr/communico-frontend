7764305964dbd0cd20772eab2e9b8f3ac423f40b
Asher <asheressani@gmail.com>
2024-12-29 22:17:29 +0500

    Updated env file!

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,8 +1,9 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:socket_io_client/socket_io_client.dart';
 
 const localUrl = "http://localhost:5000";
-const baseUrl = "https://communico.as3hr.dev";
-const baseApiUrl = "$baseUrl/api";
+final baseUrl = dotenv.env["base_url"] ?? localUrl;
+final baseApiUrl = "$baseUrl/api";
 const tokenKey = "API_TOKEN_KEY";
 var socket = io(baseUrl, {
   'transports': ['websocket'],
--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'presentation/communico.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
+  await dotenv.load(fileName: "dotenv");
   await ServiceLocator.configureServiceLocator();
   runApp(const Communico());
 }
