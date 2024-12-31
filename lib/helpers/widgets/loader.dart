c97c16636fa95d8dc9e1cb7194c915e0d6c4f29f
Asher <asheressani@gmail.com>
2024-12-31 12:42:16 +0500

    Updated loader!

--- lib/helpers/widgets/loader.dart
+++ lib/helpers/widgets/loader.dart
@@ -8,7 +8,7 @@ class Loader extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Center(
         child: CircularProgressIndicator(
-      color: AppColor.styleColor,
+      color: AppColor.primaryColor,
     ));
   }
 }
