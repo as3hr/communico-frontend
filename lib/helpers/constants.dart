c69c0bbb3d271b8516aedba218ab6f5d9b1a4197
Asher <asheressani@gmail.com>
2025-01-04 04:03:10 +0500

    finalized splash

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,9 +1,8 @@
 import 'package:socket_io_client/socket_io_client.dart';
-// import '../config.dart';
+import '../config.dart';
 
 const localUrl = "http://localhost:5000";
-const baseUrl = localUrl;
-// Config.baseUrl;
+const baseUrl = Config.baseUrl;
 const baseApiUrl = "$baseUrl/api";
 const tokenKey = "API_TOKEN_KEY";
 var socket = io(baseUrl, {
