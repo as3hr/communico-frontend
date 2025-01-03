7dd3319795cb004b27af8838471efd7b5f0b0e53
Asher <asheressani@gmail.com>
2025-01-04 03:37:43 +0500

    LoadTime improvement:6

--- lib/presentation/communico.dart
+++ lib/presentation/communico.dart
@@ -1,6 +1,5 @@
 import 'package:communico_frontend/navigation/app_navigation.dart';
 import 'package:communico_frontend/navigation/route_generator.dart';
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -10,32 +9,17 @@ class Communico extends StatelessWidget {
   const Communico({super.key});
   @override
   Widget build(BuildContext context) {
-    return DevicePreview(
-        enabled: false,
-        backgroundColor: Colors.white,
-        defaultDevice: Devices.ios.iPhone13ProMax,
-        isToolbarVisible: true,
-        tools: const [
-          DeviceSection(
-            model: true,
-            orientation: false,
-            frameVisibility: false,
-            virtualKeyboard: false,
-          ),
-        ],
-        builder: (context) {
-          return ScreenUtilInit(
-              useInheritedMediaQuery: true,
-              designSize: const Size(300, 200),
-              builder: (context, _) {
-                return MaterialApp(
-                  title: 'Communico',
-                  theme: AppTheme.theme(),
-                  debugShowCheckedModeBanner: false,
-                  navigatorKey: AppNavigation.navigatorKey,
-                  onGenerateRoute: generateRoute,
-                );
-              });
+    return ScreenUtilInit(
+        useInheritedMediaQuery: true,
+        designSize: const Size(300, 200),
+        builder: (context, _) {
+          return MaterialApp(
+            title: 'Communico',
+            theme: AppTheme.theme(),
+            debugShowCheckedModeBanner: false,
+            navigatorKey: AppNavigation.navigatorKey,
+            onGenerateRoute: generateRoute,
+          );
         });
   }
 }
